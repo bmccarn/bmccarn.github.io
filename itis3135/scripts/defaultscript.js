2c5af6bb7cfddc32dba37ecc96934f76c0172289
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-02-11 16:34:46 -0500

    updates made

--- itis3135/scripts/defaultscript.js
+++ itis3135/scripts/defaultscript.js
@@ -3,8 +3,8 @@ function scriptTest() {
 }
 
 function welcomeMessage() {
-    let name = document.getElementById("name");
-    let feeling = document.getElementById("feeling");
+    let name = document.getElementById("name").value;
+    let feeling = document.getElementById("feeling").value;
     let message = "Welcome to my website, " + name + "! " + "I'm glad you're " + feeling + ". Feel free to look around!";
     document.getElementById("outputMessage").innerHTML = message;
 }
