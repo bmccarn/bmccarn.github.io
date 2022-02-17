6e5387ab480c2a22d2f57f445f4d1bcc303e7edb
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-02-17 12:47:30 -0500

    Update polygons.js

--- itis3135/scripts/polygons.js
+++ itis3135/scripts/polygons.js
@@ -7,54 +7,51 @@ function polygonCalculator() {
 
     let numSides = Math.round(Math.abs(userInput));
 
-    //let polygon;
 
     switch (numSides) {
         case 1:
-            polygon = "This is called a monogon!";
+            alert("This is called a monogon!");
             break;
 
         case 2:
-            polygon = "This is called a bigon!";
+            alert("This is called a bigon!");
             break;
 
         case 3:
-            polygon = "This is called a triangle!";
+            alert("This is called a triangle!");
             break;
 
         case 4:
-            polygon = "This is called a quadrilateral!";
+            alert("This is called a quadrilateral!");
             break;
 
         case 5:
-            polygon = "This is called a pentagon!";
+            alert("This is called a pentagon!");
             break;
 
         case 6:
-            polygon = "This is called a hexagon!";
+            alert("This is called a hexagon!");
             break;
 
         case 7:
-            polygon = "This is called a septagon!";
+            alert("This is called a septagon!");
             break;
 
         case 8:
-            polygon = "This is called a octagon!";
+            alert("This is called a octagon!");
             break;
 
         case 9:
-            polygon = "This is called a nonagon!";
+            alert("This is called a nonagon!");
             break;
 
         case 10:
-            polygon = "This is called a decagon!";
+            alert("This is called a decagon!");
             break;
 
         default:
-            polygon = "Input Error: Please enter a number 1-10.";
+            alert("Input Error: Please enter a number 1-10.");
             polygonCalculator();
             break;
     }
-
-    alert(polygon);
 }
