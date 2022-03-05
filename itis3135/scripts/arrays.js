8fcb58ef46128d1541c219172183463eaefe53e3
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-03-05 17:32:04 -0500

    arrays

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,2 +1,9 @@
+
+
 let person = [];
 let salaries = [];
+
+function addSalary() {
+    let input = parseFloat(document.getElementById("salary").value);
+    let name = document.getElementById("employee").value;
+}
