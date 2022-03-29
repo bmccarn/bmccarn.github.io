104989069ce63093bdd2e8f02a27f1eb4ea078e3
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-03-29 19:24:59 -0400

    Update mccarn_activity10.js

--- itis3135/activity10/mccarn_activity10.js
+++ itis3135/activity10/mccarn_activity10.js
@@ -1,36 +1,40 @@
 $(document).ready(function () {
 
+        // preload the image for each link
+        $("#image_list a").each(function () {
+                var swappedImage = new Image();
+                swappedImage.src = $(this).attr("href");
+        });
 
-
+        // set up event handlers for links  
         $("#image_list a").click(function (evt) {
 
-
-
-                evt.preventDefault();
+                // get the image URL and caption for each image and animate the caption
 
                 var imageURL = $(this).attr("href");
 
+                $("#image").attr("src", imageURL);
+
                 var caption = $(this).attr("title");
 
-                $("#caption").fadeOut(1000, function () {
+                $("#caption").fadeOut(3000, function () {
 
-                        $("#caption").text(caption).fadeIn(1000);
+                        $("#caption").text(caption).fadeIn(3000);
 
+                        $("#caption").animate({ fontSize: "2.0em" }, 2000);
                 });
 
-                $("#image").fadeOut(1000, function () {
+                $("#image").fadeOut(3000, function () {
 
-                        $("#image").attr("src", imageURL).fadeIn(1000);
-
+                        $("#image").attr("src", imageURL).fadeIn(3000);
                 });
 
-        });
+                // cancel the default action of each link
+                evt.preventDefault();
 
+        }); // end click
 
+        // move the focus to the first link
         $("li:first-child a").focus();
 
-        $("#image").hide().fadeIn(1000);
-
-        $("#caption").hide().fadeIn(1000);
-
-});
+}); // end ready
