d79cba957555200101ce0a75856a5c172468e60a
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-04-06 13:36:33 -0400

    changes

--- itis3135/scripts/defaultscript.js
+++ itis3135/scripts/defaultscript.js
@@ -1,10 +1,8 @@
-function scriptTest() 
-{
+function scriptTest() {
     alert("Hey my script is running!");
 }
 
-function dateAndTime() 
-{
+function dateAndTime() {
     let date = new Date();
     let dayName = getDayName(date.getDay());
     let dayNumber = date.getDate();
@@ -12,80 +10,72 @@ function dateAndTime()
     let year = date.getFullYear();
     let time = date.toLocaleTimeString();
     let stringTime = "Today is " + time + " on " + dayName + ", " + dayNumber + " " + monthName + ", " + year;
-    var intervalId = window.setInterval(function(){
+    var intervalId = window.setInterval(function () {
         dateAndTime();
-      }, 1000);
-    document.getElementById("dateOutput").innerHTML = stringTime;  
+    }, 1000);
+    document.getElementById("dateOutput").innerHTML = stringTime;
 }
 
-function getMonthName(month) 
-{
+function getMonthName(month) {
     var arr1 = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
     return arr1[month];
 }
 
-function getDayName(day) 
-{
+function getDayName(day) {
     var arr2 = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
     return arr2[day];
 }
 
-function welcomeMessage() 
-{
+function welcomeMessage() {
     let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
     let message = "Welcome to my website, " + name + "! " + "I'm glad you're " + feeling + ". Feel free to look around!";
     document.getElementById("outputMessage").innerHTML = message;
 }
 
-function quoteGenerator() 
-{
-    let quotes = 
-    [
-        '"When you have a dream, you’ve got to grab it and never let go." -Carol Burnett', 
-        '"There is nothing impossible to they who will try." — Alexander the Great',
-        '"The bad news is time flies. The good news is you’re the pilot." — Michael Altshuler',
-        '"Keep your face always toward the sunshine, and shadows will fall behind you." — Walt Whitman',
-        '"What lies behind you and what lies in front of you, pales in comparison to what lies inside of you." — Ralph Waldo Emerson',
-        '"Belief creates the actual fact." — William James',
-        '"It is during our darkest moments that we must focus to see the light." — Aristotle',
-        '"Believe you can and you’re halfway there." – Theodore Roosevelt',
-        '"In a gentle way, you can shake the world." — Mahatma Gandhi',
-        '"Learning how to be still, to really be still and let life happen—that stillness becomes a radiance.” — Morgan Freeman'
-    ]
+function quoteGenerator() {
+    let quotes =
+        [
+            '"When you have a dream, you’ve got to grab it and never let go." -Carol Burnett',
+            '"There is nothing impossible to they who will try." — Alexander the Great',
+            '"The bad news is time flies. The good news is you’re the pilot." — Michael Altshuler',
+            '"Keep your face always toward the sunshine, and shadows will fall behind you." — Walt Whitman',
+            '"What lies behind you and what lies in front of you, pales in comparison to what lies inside of you." — Ralph Waldo Emerson',
+            '"Belief creates the actual fact." — William James',
+            '"It is during our darkest moments that we must focus to see the light." — Aristotle',
+            '"Believe you can and you’re halfway there." – Theodore Roosevelt',
+            '"In a gentle way, you can shake the world." — Mahatma Gandhi',
+            '"Learning how to be still, to really be still and let life happen—that stillness becomes a radiance.” — Morgan Freeman'
+        ]
 
-    let randomNumber = Math.floor(Math.random()*(quotes.length));
+    let randomNumber = Math.floor(Math.random() * (quotes.length));
     document.getElementById("quoteOutput").innerHTML = quotes[randomNumber];
 }
 
-function add() 
-{
+function add() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1+num2;
+    let result = +num1 + +num2;
     document.getElementById("result").innerHTML = "The answer is: " + result;
 }
 
-function subtract() 
-{
+function subtract() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1-num2;
+    let result = num1 - num2;
     document.getElementById("result").innerHTML = "The answer is: " + result;
 }
 
-function multiply() 
-{
+function multiply() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1*num2;
+    let result = num1 * num2;
     document.getElementById("result").innerHTML = "The answer is: " + result;
 }
 
-function divide() 
-{
+function divide() {
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
-    let result = num1/num2;
+    let result = num1 / num2;
     document.getElementById("result").innerHTML = "The answer is: " + result;
 }
