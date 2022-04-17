7df174e8af8fa74e8d560582ebc21c619a39a14a
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-04-17 09:17:50 -0400

    Update mccarn_activity14.js

--- itis3135/activity14/mccarn_activity14.js
+++ itis3135/activity14/mccarn_activity14.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
         $.get(title + ".json", function (data, status) {
             data = data['speakers'][0];
             $("main h1").html(data['title']);
-            $("main h2").html(data['month'] + "<br/>" + data['speaker']);
+            $("main h2").html(data['month']);
+            $("main h3").html(data['speaker']);
             $("main img").attr("src", data.image);
             $("main p").html(data.text);
         });
