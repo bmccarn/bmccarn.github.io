8095444cf9ac7bfd73eb9bf843ff17bb6085293d
bmccarn <97690865+bmccarn@users.noreply.github.com>
2022-04-20 12:17:06 -0400

    changes

---
+++ itis3135/project/project_scripts/submit_message.js
@@ -0,0 +1,12 @@
+function contactUsMessage() {
+    let name = document.getElementById("contact_fname").value;
+    alert("Thanks for reaching out, " + name + "! " + "We'll be in touch soon.");
+}
+
+function newsLetterMessage() {
+    alert("You are now subscribed!");
+}
+
+function scheduleMessage() {
+    alert("Your session is scheduled, see you soon!");
+}
